a30b3210c0e1685e8673c5f6359fb866b8603444
will <wilmo79@gmail.com>
2019-06-08 13:47:43 -0400

    adding a candlestick chart

--- hello-world.R
+++ hello-world.R
@@ -3,8 +3,29 @@
 library(dplyr)
 library(ggplot2)
 library(readr)
+library(tidyquant)  
 
 df <- read_csv("https://markets.cboe.com/us/futures/market_statistics/historical_data/products/csv/VX/2020-01-22")
 
 glimpse(df)
+df <- rename(df, date=`Trade Date`)
+
+# ht to https://cran.r-project.org/web/packages/tidyquant/vignettes/TQ04-charting-with-tidyquant.html
+df %>%
+  filter(Close != 0) %>%
+  ggplot(aes(x = date, y = Close)) +
+    geom_line() +
+    labs(title = "CBOE Line Chart", y = "Closing Price", x = "") + 
+    theme_tq()
+
+df %>%
+  filter(Close != 0) %>%
+  ggplot(aes(x = date, y = Close)) +
+    geom_candlestick(aes(open = Open, high = High, low = Low, close = Close),
+                     color_up = "darkblue", color_down = "red") +
+    labs(title = "CBOE Candlestick Chart", y = "Closing Price", x = "") +
+    theme_tq()
+
+
+
 
